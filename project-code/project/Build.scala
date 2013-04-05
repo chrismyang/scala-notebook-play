b4f451dfb8f98dfe847afb23e29565a6da592dbe
Chris Yang <chris.m.yang@gmail.com>
2013-04-04 23:55:29 -0700

    Adding scala notebook server as a locally published dependency (obviously won't work for anyone else yet).

--- project-code/project/Build.scala
+++ project-code/project/Build.scala
@@ -4,15 +4,15 @@ import PlayProject._
 
 object ApplicationBuild extends Build {
 
-    val appName         = "scala-notebook-play"
-    val appVersion      = "1.0-SNAPSHOT"
+  val appName         = "scala-notebook-play"
+  val appVersion      = "1.0-SNAPSHOT"
 
-    val appDependencies = Seq(
-      // Add your project dependencies here,
-    )
+  val appDependencies = Seq(
+    "com.bwater" % "notebook-server_2.9.2" % "0.3.0-SNAPSHOT"
+  )
 
-    val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
-      // Add your own project settings here      
-    )
+  val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
+    resolvers += "Local SBT Repository" at "file:///Users/chris/.ivy2/local"
+  )
 
 }
