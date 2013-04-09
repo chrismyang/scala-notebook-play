9e0b322074df6888215d3dfff1de444628ceffa1
Chris Yang <chris.m.yang@gmail.com>
2013-04-09 15:44:16 -0700

    Added routes for listing notebooks and getting cluster status.

--- project-code/app/controllers/NotebookController.scala
+++ project-code/app/controllers/NotebookController.scala
@@ -1,11 +1,27 @@
 package controllers
 
 import play.api.mvc.{Action, Controller}
+import com.bwater.notebook.server.{ScalaNotebookConfig, NotebookSession}
+import play.api.libs.json.Json
 
-object NotebookController extends Controller {
+object NotebookController extends Controller with NotebookSession {
+
+
+
   def index = Action { implicit request =>
-    val projectName = "TODO Project Name"
+    val projectName = "."
     Ok(views.html.projectdashboard(projectName))
   }
 
+  def listNotebooks = Action { implicit request =>
+    val jsonString = unfiltered.response.Json.jsonToString(nbm.listNotebooks)
+    Ok(Json.parse(jsonString))
+  }
+
+  def clusters = Action { implicit request =>
+    val s = """[{"profile":"default","status":"stopped","profile_dir":"C:\\Users\\Ken\\.ipython\\profile_default"}]"""
+    Ok(Json.parse(s))
+  }
+
+  protected def config = ScalaNotebookConfig.defaults
 }
