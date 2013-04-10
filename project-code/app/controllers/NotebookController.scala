6a1e8d56d3b5f2544f53b44ebaca925a783198b7
Chris Yang <chris.m.yang@gmail.com>
2013-04-09 19:21:48 -0700

    First cut at notebook endpoint.  Basically just copied the SSP template over from SN, will start to hack away at it to get it to work.

--- project-code/app/controllers/NotebookController.scala
+++ project-code/app/controllers/NotebookController.scala
@@ -6,7 +6,8 @@ import play.api.libs.json.Json
 
 object NotebookController extends Controller with NotebookSession {
 
-
+  val domain = "127.0.0.1"
+  val port = 9000
 
   def index = Action { implicit request =>
     val projectName = "."
@@ -21,6 +22,13 @@ object NotebookController extends Controller with NotebookSession {
   def clusters = Action { implicit request =>
     val s = """[{"profile":"default","status":"stopped","profile_dir":"C:\\Users\\Ken\\.ipython\\profile_default"}]"""
     Ok(Json.parse(s))
+  }
+
+  def view(name: String) = Action { implicit request =>
+    val id = request.queryString.get("id").flatMap(_.headOption).getOrElse(nbm.notebookId(name))
+    val wsUrl = "ws:/%s:%d".format(domain, port)
+
+    Ok(views.html.notebook(nbm.name, id, name, wsUrl))
   }
 
   protected def config = ScalaNotebookConfig.defaults
